ba25cae561e0078f431c13f93927feb00bcfc19d
Anduin Mooney <anduinmooney@gmail.com>
2017-12-15 15:12:45 -0800

    Add variables for toppings and size.

---
+++ Desktop/Pizza-order/js/scripts.js
@@ -0,0 +1,14 @@
+// business logic
+var small = 6;
+var medium = 8;
+var large = 12;
+
+var pepperoni = 2;
+var sausage = 2;
+var ham = 2;
+var bacon = 1;
+
+var olive = 1;
+var onion = 1;
+var pineapple = 2;
+var spinach = 1;
