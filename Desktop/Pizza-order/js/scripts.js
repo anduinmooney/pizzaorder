35183d7c53a88895c8d40d9acedafacbfb1d2dec
Anduin Mooney <anduinmooney@gmail.com>
2017-12-15 15:37:00 -0800

    Add ability to let user go through entire webpage.

--- Desktop/Pizza-order/js/scripts.js
+++ Desktop/Pizza-order/js/scripts.js
@@ -12,3 +12,23 @@ var olive = 1;
 var onion = 1;
 var pineapple = 2;
 var spinach = 1;
+
+
+// user logic
+$(document).ready(function() {
+  $("#pizzasize").submit(function(event) {
+    event.preventDefault();
+    $("#pizzasize").hide();
+    $("#pizzatopping").fadeIn();
+    });
+  $("#pizzatopping").submit(function(event) {
+    event.preventDefault();
+    $("#pizzatopping").hide();
+    $("#order").fadeIn();
+  });
+  $("#order").submit(function(event) {
+    event.preventDefault();
+    $("#order").hide();
+    $("#ordertotal").fadeIn();
+  });
+});
