da1b79847cdf9015076e4aec655c57a7f7ab0d80
Anduin Mooney <anduinmooney@gmail.com>
2017-12-15 16:49:21 -0800

    Record user input for pizza size.

--- Desktop/Pizza-order/js/scripts.js
+++ Desktop/Pizza-order/js/scripts.js
@@ -2,9 +2,9 @@
 var size = {small:6, medium:8, large:12};
 var topping ={pepperoni:2, sausage:2, ham:2, bacon:1, olive:1, onion:1, pineapple:2, spinach:1};
 
-// var small = 6;
-// var medium = 8;
-// var large = 12;
+var small = 6;
+var medium = 8;
+var large = 12;
 //
 // var pepperoni = 2;
 // var sausage = 2;
@@ -16,6 +16,24 @@ var topping ={pepperoni:2, sausage:2, ham:2, bacon:1, olive:1, onion:1, pineappl
 // var pineapple = 2;
 // var spinach = 1;
 
+function pizza(size) {
+  this.size = size;
+  this.topping= topping;
+}
+
+pizza.prototype.pizzaSizePrice = function() {
+  if(this.size === "small") {
+    var sizePrice = small;
+  } else if(this.size ==="medium") {
+    var sizePrice = medium;
+  } else {
+     var sizePrice = large;
+   }
+  console.log(sizePrice);
+  return sizePrice;
+
+}
+
 
 // user logic
 $(document).ready(function() {
@@ -23,15 +41,24 @@ $(document).ready(function() {
     event.preventDefault();
     $("#pizzasize").hide();
     $("#pizzatopping").fadeIn();
+    var inputtedSize = $("input:radio[name=pizza-size]:checked").val();
+    var newPizza = new pizza(inputtedSize);
+    var newPizzaPrice = newPizza.pizzaSizePrice();
+
+
     });
   $("#pizzatopping").submit(function(event) {
     event.preventDefault();
     $("#pizzatopping").hide();
     $("#order").fadeIn();
+    // var inputtedToppings = $("checkbox[name=topping]:checked").val();
   });
   $("#order").submit(function(event) {
     event.preventDefault();
     $("#order").hide();
     $("#ordertotal").fadeIn();
+
+
+
   });
 });
